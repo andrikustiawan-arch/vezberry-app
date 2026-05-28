3ac07fc28741f6c49fc8b12bb972ed7dd5ef316b
Andri Kustiawan <andri.kustiawan@gmail.com>
2026-05-28 08:16:21 +0700

    enable frontend

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-    res.send("VEZBERRY ROOT OK");
-});
-
 app.get("/test", (req, res) => {
     res.send("TEST OK");
 });
